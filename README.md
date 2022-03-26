2ceb9169fa9ea155f515e9690e2cd546154c2471
WOo0W <24245520+WOo0W@users.noreply.github.com>
2022-03-26 23:00:24 +0800

    feat: impl Stream for Pager

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 # pixivcrab
 
-A pixiv AppAPI bindings in Rust.
+A pixiv AppAPI in Rust.
 
 ## Example
 
 ```rust
-use pixivcrab::{AppAPI, AuthMethod};
-use reqwest::Proxy;
+use pixivcrab::{AppApi, AuthMethod};
+use futures::TryStreamExt;
 
 #[tokio::main]
 async fn main() {
-    let client_builder =
-        reqwest::Client::builder().proxy(Proxy::http("http://127.0.0.1:8080").unwrap());
-    let api = AppAPI::new(
-        AuthMethod::RefreshToken("refresh_token12345".to_string()),
+    let api = AppApi::new(
+        AuthMethod::RefreshToken("your_refresh_token".to_string()),
         "en",
-        client_builder,
+        reqwest::Client::builder(),
     )
     .unwrap();
+    let user = api.user_detail("123456").await.unwrap();
+    println!("{:?}", user);
     let mut pager = api.illust_bookmarks("123456", false);
-    while let Some(r) = pager.next().await.unwrap() {
+    // `Pager` implements `futures::Stream`.
+    // Import `futures::TryStreamExt` to use `try_next` method.
+    while let Some(r) = pager.try_next().await.unwrap() {
         for i in r.illusts {
             println!("{} {:?}", i.title, i.tags);
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,49 @@
-use std::{fmt::Debug, marker::PhantomData};
-
-use futures::lock::Mutex;
+//! A pixiv AppAPI in Rust.
+//! 
+//! # Example
+//!
+//! ```no_run
+//! use pixivcrab::{AppApi, AuthMethod};
+//! use futures::TryStreamExt;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let api = AppApi::new(
+//!         AuthMethod::RefreshToken("your_refresh_token".to_string()),
+//!         "en",
+//!         reqwest::Client::builder(),
+//!     )
+//!     .unwrap();
+//!     let user = api.user_detail("123456").await.unwrap();
+//!     println!("{:?}", user);
+//!     let mut pager = api.illust_bookmarks("123456", false);
+//!     // `Pager` implements `futures::Stream`.
+//!     // Import `futures::TryStreamExt` to use `try_next` method.
+//!     while let Some(r) = pager.try_next().await.unwrap() {
+//!         for i in r.illusts {
+//!             println!("{} {:?}", i.title, i.tags);
+//!         }
+//!     }
+//! }
+//! ```
 
 use chrono::{DateTime, Duration, Utc};
+use futures::{future::BoxFuture, lock::Mutex, Future, Stream};
 use models::{auth, illust, novel, user};
 use reqwest::{header::HeaderMap, ClientBuilder};
 use serde::de::DeserializeOwned;
 use snafu::ResultExt;
+use std::{
+    fmt::Debug,
+    marker::PhantomData,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
+
+#[macro_use]
+extern crate derivative;
+
 pub mod error;
 pub mod models;
 
@@ -18,47 +55,108 @@ pub enum AuthMethod {
 }
 
 pub trait NextUrl {
-    fn next_url(&self) -> Option<&str>;
+    fn next_url(&self) -> Option<String>;
 }
 
 macro_rules! impl_next_url {
     ($t:ident) => {
         impl NextUrl for $t {
-            fn next_url(&self) -> Option<&str> {
-                self.next_url.as_ref().map(|x| x.as_str())
+            fn next_url(&self) -> Option<String> {
+                self.next_url.clone()
             }
         }
     };
 }
 pub(crate) use impl_next_url;
 
-#[derive(Debug)]
-pub struct Pager<'a, T>
+/// A [`Pager`] streams the results from `next_url`.
+/// It iterates over the pages until the `next_url` is None.
+///
+/// ```no_run
+/// use pixivcrab::{AppApi, AuthMethod};
+/// use futures::TryStreamExt;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let api = AppApi::new(
+///         AuthMethod::RefreshToken("your_refresh_token".to_string()),
+///         "en",
+///         reqwest::Client::builder(),
+///     )
+///     .unwrap();
+///     let mut pager = api.illust_bookmarks("123456", false);
+///     while let Some(r) = pager.try_next().await.unwrap() {
+///         for i in r.illusts {
+///             println!("{} {:?}", i.title, i.tags);
+///         }
+///     }
+/// }
+/// ```
+#[derive(Derivative)]
+#[derivative(Debug)]
+pub struct Pager<T>
 where
-    T: DeserializeOwned + NextUrl + Debug,
+    T: DeserializeOwned + NextUrl + Send,
 {
-    app_api: &'a AppAPI,
+    app_api: AppApi,
     next_url: Option<String>,
-    response_type: PhantomData<T>,
+    _response_type: PhantomData<fn() -> T>,
+    #[derivative(Debug = "ignore")]
+    future: Option<BoxFuture<'static, Result<T>>>,
 }
 
-impl<T: DeserializeOwned + NextUrl + Debug> Pager<'_, T> {
-    pub async fn next(&mut self) -> Result<Option<T>> {
-        match &self.next_url {
-            Some(url) => {
-                let r = self
-                    .app_api
-                    .send_authorized(self.app_api.client.get(url))
-                    .await?;
-                match self.app_api.parse_json::<T>(r).await {
-                    Ok(r) => {
-                        self.next_url = r.next_url().map(|x| x.to_string());
-                        Ok(Some(r))
+impl<T> Pager<T>
+where
+    T: DeserializeOwned + NextUrl + Send,
+{
+    fn new(app_api: AppApi, next_url: String) -> Self {
+        Self {
+            app_api,
+            next_url: Some(next_url),
+            _response_type: PhantomData,
+            future: None,
+        }
+    }
+}
+
+impl<T> Stream for Pager<T>
+where
+    T: DeserializeOwned + NextUrl + Send,
+{
+    type Item = Result<T>;
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        loop {
+            match self.future {
+                Some(ref mut f) => {
+                    return match Pin::new(f).poll(cx) {
+                        Poll::Ready(r) => {
+                            self.future = None;
+                            match r {
+                                Ok(t) => {
+                                    self.next_url = t.next_url();
+                                    Poll::Ready(Some(Ok(t)))
+                                }
+                                Err(e) => Poll::Ready(Some(Err(e))),
+                            }
+                        }
+                        Poll::Pending => Poll::Pending,
                     }
-                    Err(e) => Err(e),
                 }
+                None => match self.next_url.take() {
+                    Some(next_url) => {
+                        let app_api = self.app_api.clone();
+                        self.future = Some(Box::pin(async move {
+                            let r = app_api
+                                .send_authorized(app_api.0.client.get(next_url))
+                                .await?;
+                            app_api.parse_json::<T>(r).await
+                        }));
+                    }
+                    None => {
+                        return Poll::Ready(None);
+                    }
+                },
             }
-            None => Ok(None),
         }
     }
 }
@@ -71,19 +169,64 @@ fn restrict(private: bool) -> &'static str {
     }
 }
 
-#[derive(Debug)]
-pub struct AppAPI {
-    pub client: reqwest::Client,
-    pub auth_method: AuthMethod,
+#[derive(Debug, Clone)]
+pub struct AppApi(Arc<AppApiInner>);
 
+#[derive(Debug, Clone)]
+pub struct AppApiConfig {
     pub base_url: String,
-
     pub hash_secret: String,
     pub device_token: String,
     pub client_id: String,
     pub client_secret: String,
-
     pub auth_url: String,
+    pub base_headers: HeaderMap,
+    pub timeout: std::time::Duration,
+    pub skip_config_client: bool,
+    pub user_agent: String,
+}
+
+impl Default for AppApiConfig {
+    fn default() -> Self {
+        let mut base_headers = HeaderMap::new();
+        base_headers.insert("Accept-Language", "en".parse().unwrap());
+        base_headers.insert("App-OS", "ios".parse().unwrap());
+        base_headers.insert("App-OS-Version", "12.4.5".parse().unwrap());
+        base_headers.insert("App-Version", "7.8.16".parse().unwrap());
+
+        Self {
+            base_url: "https://app-api.pixiv.net".to_string(),
+            auth_url: "https://oauth.secure.pixiv.net/auth/token".to_string(),
+            client_id: "KzEZED7aC0vird8jWyHM38mXjNTY".to_string(),
+            client_secret: "W9JZoJe00qPvJsiyCGT3CCtC6ZUtdpKpzMbNlUGP".to_string(),
+            device_token: "ec731472f8db58afe8588cbba92d5846".to_string(),
+            hash_secret: "28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c"
+                .to_string(),
+            base_headers,
+            timeout: std::time::Duration::from_secs(15),
+            skip_config_client: false,
+            user_agent: "PixivIOSApp/7.8.16 (iOS 12.4.5; iPhone7,2)".to_string(),
+        }
+    }
+}
+
+impl AppApiConfig {
+    /// Set language used in reguest.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `language` is of invalid header value.
+    pub fn set_language(&mut self, language: &str) {
+        self.base_headers
+            .insert("Accept-Language", language.parse().unwrap());
+    }
+}
+
+#[derive(Debug)]
+struct AppApiInner {
+    client: reqwest::Client,
+    auth_method: AuthMethod,
+    config: AppApiConfig,
     auth: Mutex<Auth>,
 }
 
@@ -127,47 +270,48 @@ pub fn default_headers(hash_secret: &str) -> HeaderMap {
     h
 }
 
-impl AppAPI {
+impl AppApi {
+    /// Create new instance of AppApi.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `language` is of invalid header value.
     pub fn new(
         auth_method: AuthMethod,
         language: &str,
         client_builder: ClientBuilder,
     ) -> Result<Self> {
-        let mut base_headers = HeaderMap::new();
-        base_headers.insert(
-            "Accept-Language",
-            language.parse().context(error::HeaderParse)?,
-        );
-        base_headers.insert("App-OS", "ios".parse().unwrap());
-        base_headers.insert("App-OS-Version", "12.4.5".parse().unwrap());
-        base_headers.insert("App-Version", "7.8.16".parse().unwrap());
+        let mut config = AppApiConfig::default();
+        config.set_language(language);
+        Self::new_with_config(auth_method, client_builder, config)
+    }
 
-        Ok(Self {
-            client: client_builder
-                .user_agent("PixivIOSApp/7.8.16 (iOS 12.4.5; iPhone7,2)")
-                .timeout(std::time::Duration::from_secs(15))
-                .default_headers(base_headers)
-                .cookie_store(true)
-                .build()
-                .context(error::HTTP)?,
-            base_url: "https://app-api.pixiv.net".to_string(),
+    pub fn new_with_config(
+        auth_method: AuthMethod,
+        mut client_builder: ClientBuilder,
+        config: AppApiConfig,
+    ) -> Result<Self> {
+        if !config.skip_config_client {
+            client_builder = client_builder
+                .user_agent(&config.user_agent)
+                .timeout(config.timeout)
+                .default_headers(config.base_headers.clone())
+                .cookie_store(true);
+        }
+        Ok(Self(Arc::new(AppApiInner {
+            client: client_builder.build().context(error::HTTP)?,
             auth: Mutex::new(Auth::default()),
-            auth_url: "https://oauth.secure.pixiv.net/auth/token".to_string(),
             auth_method,
-            client_id: "KzEZED7aC0vird8jWyHM38mXjNTY".to_string(),
-            client_secret: "W9JZoJe00qPvJsiyCGT3CCtC6ZUtdpKpzMbNlUGP".to_string(),
-            device_token: "ec731472f8db58afe8588cbba92d5846".to_string(),
-            hash_secret: "28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c"
-                .to_string(),
-        })
+            config,
+        })))
     }
 
     pub fn headers(&self) -> HeaderMap {
-        default_headers(&self.hash_secret)
+        default_headers(&self.0.config.hash_secret)
     }
 
     pub async fn auth(&self) -> Result<AuthResult> {
-        let mut auth = self.auth.lock().await;
+        let mut auth = self.0.auth.lock().await;
         if !auth.access_token_expired() {
             return Ok(AuthResult {
                 // Because the token is not expired, access_token and refresh_token are considered set here.
@@ -177,9 +321,9 @@ impl AppAPI {
             });
         }
         let mut form = auth::Form {
-            client_id: &self.client_id,
-            client_secret: &self.client_secret,
-            device_token: &self.device_token,
+            client_id: &self.0.config.client_id,
+            client_secret: &self.0.config.client_secret,
+            device_token: &self.0.config.device_token,
             get_secure_url: true,
             include_policy: true,
             grant_type: None,
@@ -191,7 +335,7 @@ impl AppAPI {
             form.grant_type = Some("refresh_token");
             form.refresh_token = Some(r);
         } else {
-            match &self.auth_method {
+            match &self.0.auth_method {
                 AuthMethod::RefreshToken(r) => {
                     form.grant_type = Some("refresh_token");
                     form.refresh_token = Some(r);
@@ -199,8 +343,9 @@ impl AppAPI {
             }
         }
         let r = self
+            .0
             .client
-            .post(&self.auth_url)
+            .post(&self.0.config.auth_url)
             .headers(self.headers())
             .form(&form)
             .send()
@@ -247,73 +392,62 @@ impl AppAPI {
     {
         let status = response.status();
         if status.is_success() {
-            response.json::<T>().await.context(error::HTTP)
+            response.json().await.context(error::HTTP)
         } else {
             error::PixivStatusCode { code: status }.fail()
         }
     }
 
-    pub fn illust_bookmarks<'a>(
-        &'a self,
-        user_id: &str,
-        private: bool,
-    ) -> Pager<'a, illust::Response> {
-        Pager {
-            app_api: self,
-            next_url: Some(format!(
+    pub fn illust_bookmarks(&self, user_id: &str, private: bool) -> Pager<illust::Response> {
+        Pager::new(
+            self.clone(),
+            format!(
                 "{}/v1/user/bookmarks/illust?restrict={}&user_id={}",
-                self.base_url,
+                self.0.config.base_url,
                 restrict(private),
                 user_id
-            )),
-            response_type: PhantomData,
-        }
+            ),
+        )
     }
 
-    pub fn illust_uploads<'a>(&'a self, user_id: &str) -> Pager<'a, illust::Response> {
-        Pager {
-            app_api: self,
-            next_url: Some(format!(
+    pub fn illust_uploads(&self, user_id: &str) -> Pager<illust::Response> {
+        Pager::new(
+            self.clone(),
+            format!(
                 "{}/v1/user/illusts?user_id={}",
-                self.base_url, user_id
-            )),
-            response_type: PhantomData,
-        }
+                self.0.config.base_url, user_id
+            ),
+        )
     }
 
-    pub fn novel_bookmarks<'a>(
-        &'a self,
-        user_id: &str,
-        private: bool,
-    ) -> Pager<'a, novel::Response> {
-        Pager {
-            app_api: self,
-            next_url: Some(format!(
+    pub fn novel_bookmarks(&self, user_id: &str, private: bool) -> Pager<novel::Response> {
+        Pager::new(
+            self.clone(),
+            format!(
                 "{}/v1/user/bookmarks/novel?restrict={}&user_id={}",
-                self.base_url,
+                self.0.config.base_url,
                 restrict(private),
                 user_id
-            )),
-            response_type: PhantomData,
-        }
+            ),
+        )
     }
 
-    pub fn novel_uploads<'a>(&'a self, user_id: &str) -> Pager<'a, novel::Response> {
-        Pager {
-            app_api: self,
-            next_url: Some(format!(
+    pub fn novel_uploads(&self, user_id: &str) -> Pager<novel::Response> {
+        Pager::new(
+            self.clone(),
+            format!(
                 "{}/v1/user/novels?user_id={}",
-                self.base_url, user_id
-            )),
-            response_type: PhantomData,
-        }
+                self.0.config.base_url, user_id
+            ),
+        )
     }
 
-    pub async fn user_detail<'a>(&'a self, user_id: &str) -> Result<user::Response> {
+    pub async fn user_detail(&self, user_id: &str) -> Result<user::Response> {
         self.parse_json(
             self.send_authorized(
-                self.client
-                    .get(format!("{}/v1/user/detail", self.base_url,))
+                self.0
+                    .client
+                    .get(format!("{}/v1/user/detail", self.0.config.base_url,))
                     .query(&[("user_id", user_id)]),
             )
             .await?,
@@ -321,11 +455,12 @@ impl AppAPI {
         .await
     }
 
-    pub async fn novel_text<'a>(&'a self, novel_id: &str) -> Result<novel::NovelTextResponse> {
+    pub async fn novel_text<'a>(&self, novel_id: &str) -> Result<novel::NovelTextResponse> {
         self.parse_json(
             self.send_authorized(
-                self.client
-                    .get(format!("{}/v1/novel/text", self.base_url))
+                self.0
+                    .client
+                    .get(format!("{}/v1/novel/text", self.0.config.base_url))
                     .query(&[("novel_id", novel_id)]),
             )
             .await?,
@@ -333,11 +468,12 @@ impl AppAPI {
         .await
     }
 
-    pub async fn ugoira_metadata<'a>(&'a self, illust_id: &str) -> Result<illust::UgoiraResponse> {
+    pub async fn ugoira_metadata<'a>(&self, illust_id: &str) -> Result<illust::UgoiraResponse> {
         self.parse_json(
             self.send_authorized(
-                self.client
-                    .get(format!("{}/v1/ugoira/metadata", self.base_url))
+                self.0
+                    .client
+                    .get(format!("{}/v1/ugoira/metadata", self.0.config.base_url))
                     .query(&[("illust_id", illust_id)]),
             )
             .await?,
